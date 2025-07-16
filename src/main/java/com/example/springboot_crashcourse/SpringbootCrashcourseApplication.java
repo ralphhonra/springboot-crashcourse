c3d9289d374480f056ac6c9c09ea90f8ca2e168b
Ralph Daniel Honra <ralphdaniel.honra@gcash.com>
2025-07-16 14:18:11 +0800

    First commit

---
+++ src/main/java/com/example/springboot_crashcourse/SpringbootCrashcourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_crashcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrashcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrashcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_crashcourse/SpringbootCrashcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_crashcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrashcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
